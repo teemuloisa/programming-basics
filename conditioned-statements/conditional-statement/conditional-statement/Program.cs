871e84b4391e13c05d8dc02757cc69adcbcc0e7c
Teemu Loisa <teemu.loisa@gmail.com>
2017-09-21 15:44:41 +0300

    conditional statement

---
+++ conditioned-statements/conditional-statement/conditional-statement/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace conditional_statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa positiivinen, negatiivinen tai nolla.");
+            Console.Write("Syötä luku: ");
+            string userInput;
+            userInput= Console.ReadLine();
+
+           // int evaluatedNumber;
+            int.TryParse(userInput, out int evaluatedNumber);
+
+            if (evaluatedNumber < 0)
+            {
+                Console.WriteLine($"Numero {evaluatedNumber} on negatiivinen.");
+                //  Console.WriteLine($"Syötit arvon {userInput}") ;
+                Console.ReadKey();
+            }
+            else if (evaluatedNumber < 0)
+            {
+                Console.WriteLine($"Numero {evaluatedNumber} on positiivinen.");
+                //  Console.WriteLine($"Syötit arvon {userInput}") ;
+                Console.ReadKey();
+            }
+
+            else ()
+            {
+                Console.WriteLine($"Numero {evaluatedNumber} on 0.");
+                //  Console.WriteLine($"Syötit arvon {userInput}") ;
+                Console.ReadKey();
+            }
+        }
+    }
+}
