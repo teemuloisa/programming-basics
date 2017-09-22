ead9e238079ff317fa16a49a31e056a7c54bf719
Teemu Loisa <teemu.loisa@gmail.com>
2017-09-22 13:55:14 +0300

    if task-2 done

---
+++ conditioned-statements/conditional-statement/task-2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Ohjelma tulostaa parillinen tai pariton.");
+            bool isNumber;
+            do
+            {
+                Console.Write("Syötä luku: ");
+                string userInput;
+                userInput = Console.ReadLine();
+
+                
+                isNumber = int.TryParse(userInput, out int evaluatedNumber);
+
+                if (isNumber == true)
+                {
+
+                    if (evaluatedNumber % 2 == 0)
+                    {
+                        Console.WriteLine($"Numero {evaluatedNumber} on parillinen.");
+
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Numero {evaluatedNumber} on pariton.");
+
+                    }
+                    
+                }
+
+                else
+                {
+                    Console.WriteLine("Oletko hieman yksinkertainen? Et syöttänyt lukua.");
+
+                }
+            } while (isNumber == false);
+            Console.ReadKey();
+        }
+    }
+}
