d1a63f2111ead15731a5f82a0e0c09f86f17ecb2
Teemu Loisa <teemu.loisa@gmail.com>
2017-10-13 10:49:11 +0300

    loop-4 tehty

---
+++ loop-statements/loop_example/loop-4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace loop_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.Write("Ohjelma laskee parillisten ja parittomien summan. Anna luku: ");
+            string userInput = Console.ReadLine();
+            int number = int.Parse(userInput);
+            //int.TryParse(userInput, out int number);
+            int i = 0;
+
+            int evenSum = 0; //Parilliset
+            int oddSum = 0; //Parittomat
+
+            if (number < 0)
+            {
+                do
+                {
+                    if (i % 2 == 0)
+                        evenSum = evenSum - -i;
+                    else
+                        oddSum = oddSum - -i;
+                    i = i + -1;
+                } while (i >= number);
+                Console.WriteLine($"Parillisten summa: {evenSum}\nParittomien summa: {oddSum}");
+
+                Console.ReadKey();
+            }
+
+            else
+            {
+                do
+                {
+                    if (i % 2 == 0)
+                        evenSum = evenSum + i;
+                    else
+                        oddSum = oddSum + i;
+                    i = i + 1;
+                } while (i <= number);
+                Console.WriteLine($"Parillisten summa: {evenSum}\nParittomien summa: {oddSum}");
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
