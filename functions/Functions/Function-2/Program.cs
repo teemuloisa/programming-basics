e2e11c0cca961e5e80d101116a6df3ad6f06a58d
Teemu Loisa <teemu.loisa@gmail.com>
2017-10-19 11:19:29 +0300

    funktio-1 tehty

---
+++ functions/Functions/Function-2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Function_2
+{
+    class Program
+    {
+        static string minimi(int n, int y)
+        {
+            if (n > y)
+            {
+                Console.WriteLine($"Luku {n} on suurempi kuin luku {y}");
+            }
+            else
+                Console.WriteLine($"Luku {y} on suurempi kuin luku {n}");
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Functions - Task 1. \n");
+
+            int n;
+            int y;
+
+            Console.Write("Syötä kaksi lukua: ");
+            n = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+
+            string vertailu = minimi(n,y);
+            Console.WriteLine(vertailu);
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ functions/Functions/Functions/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Functions
+{
+    class Program
+    {
+        static string getNAsterisks(int n)
+        {
+            string res = string.Empty;
+            for (int i = 0; i < n; i++)
+            {
+                res += "*";
+            }
+            return res;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Functions - Task 1. \n");
+
+            int n;
+            while (true)
+            {
+                Console.Write("Tähtien lukumäärä: ");
+                n = int.Parse(Console.ReadLine());
+                if (n < 0)
+                {
+                    Console.WriteLine($"Luku {n} ei ole sallittu luku");
+                }
+                else
+                {
+                    break;
+                }
+            }
+            string asterisks = getNAsterisks(n);
+            Console.WriteLine(asterisks);
+            Console.ReadKey();
+        }
+    }
+}
