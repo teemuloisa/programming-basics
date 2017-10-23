8259a0300acc7700a6cee25eda70ce8b434d3ad9
Teemu Loisa <teemu.loisa@gmail.com>
2017-10-23 10:03:24 +0300

    funktio-2 tehty

--- functions/Functions/Function-2/Program.cs
+++ functions/Functions/Function-2/Program.cs
@@ -4,15 +4,39 @@ namespace Function_2
 {
     class Program
     {
-        static string minimi(int n, int y)
-        {
+        static string Minimi(int n, int y)
+        {       
             if (n > y)
             {
-                Console.WriteLine($"Luku {n} on suurempi kuin luku {y}");
+            
+                string res = string.Empty;
+                res = ($"Luku {n} on suurempi kuin luku {y}");
+                return res;
+
+            }
+            else if (y > n)
+            {
+           
+                string res = string.Empty;
+                res = ($"Luku {y} on suurempi kuin luku {n}");
+                return res;
+            }
+
+            else if (y == n)
+            {
+
+                string res = string.Empty;
+                res = ($"Luku {y} on suurempi kuin luku {n}");
+                return res;
             }
             else
-                Console.WriteLine($"Luku {y} on suurempi kuin luku {n}");
-
+            {
+             
+                string res = string.Empty;
+                res = ("Et syöttänyt lukua.");
+                return res;
+            }
+            
         }
 
 
@@ -20,16 +44,23 @@ namespace Function_2
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-            Console.WriteLine("Functions - Task 1. \n");
+            Console.WriteLine("Functions - Task 2. \n");
+            Console.WriteLine("Syötä kaksi lukua.");
 
             int n;
             int y;
 
-            Console.Write("Syötä kaksi lukua: ");
+            Console.Write("Luku 1: ");
             n = int.Parse(Console.ReadLine());
+
+
+
+            Console.Write("Luku 2: ");
             y = int.Parse(Console.ReadLine());
 
-            string vertailu = minimi(n,y);
+
+
+            string vertailu = Minimi(n, y);
             Console.WriteLine(vertailu);
             Console.ReadKey();
 
