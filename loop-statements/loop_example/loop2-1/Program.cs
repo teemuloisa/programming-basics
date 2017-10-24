3fb5e84143d86e86e8a39bb87397cf41586b905e
Teemu Loisa <teemu.loisa@gmail.com>
2017-10-24 10:02:44 +0300

    loop2-2 tehty

---
+++ loop-statements/loop_example/loop2-1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace loop2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            do
+            {
+                Console.WriteLine($"{i}\t\t{Math.Round(Math.Sqrt(i), 2)}"); 
+                
+                i = i + 1;
+            } while (i <= 10);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ loop-statements/loop_example/loop2-2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace loop2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            int n = 1;
+            do
+            {
+                int i = 1;
+                do
+                {
+                    Console.WriteLine($"{n} x {i} = {(n * i)}");
+                    i = i + 1;
+                } while (i <= 10);
+                n = n + 1 ;
+            } while (n <= 10);
+            Console.ReadKey();
+        }
+    }
+}
