4a44a0cef18313e8009c9a0b56007cc6fcdf6afd
Teemu Loisa <teemu.loisa@gmail.com>
2017-12-07 14:08:47 +0200

    array 3 kesken

---
+++ arrays/Array/Array-3-1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Array_3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+            Random rnd = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(20);
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"[{i}] = {array[i]} \n");
+            }
+
+            Console.ReadKey();
+        }
+
+        static void Muunnos()
+        {
+
+        }
+    }
+}
---
+++ arrays/Array/Lotto/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lotto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[7];
+            Random rnd = new Random();
+          
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(40);
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"{array[i]} ");
+            }
+            int lisanumero = rnd.Next(40);
+            int tuplausnumero = rnd.Next(40);
+            Console.WriteLine($"Lisänumero: {lisanumero}");
+            Console.WriteLine($"Tuplausnumero: {tuplausnumero} ");
+
+            Console.ReadKey();
+        }
+    }
+}
